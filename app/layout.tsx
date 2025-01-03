0f4efbded177ce1ecf5ee81ec3d348025f6d0fdf
Rex the Breathtaker <adityakbarp@gmail.com>
2025-01-03 20:04:29 +0000

    update all

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Arimo, Source_Sans_3 } from 'next/font/google'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
+import Head from 'next/head';
 
 const arimo = Arimo({
   weight: ['400', '600', '700'],
@@ -28,6 +29,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head> <link rel="icon" href="/images/favicon.png" />
+        </Head>
       <body className={`${arimo.variable} ${sourceSans3.variable} font-sans bg-black text-white`}>
         <Navigation />
         <main>{children}</main>
