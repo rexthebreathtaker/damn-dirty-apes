28d91c2cd4803f745f39d0955d69e5019249082e
Rex the Breathtaker <adityakbarp@gmail.com>
2025-01-03 21:51:40 +0000

    update nav

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -34,7 +34,7 @@ export default function Navigation() {
               <img
                 src="/images/logo.webp"
                 alt="Logo"
-                className="h-14 w-auto"
+                className="h-16 w-auto"
               />
             </Link>
           </div>
