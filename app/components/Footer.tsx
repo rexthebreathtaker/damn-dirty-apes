2435dc9704f72928beddc4e6536501356333216c
Rex the Breathtaker <adityakbarp@gmail.com>
2025-01-04 10:21:41 +0700

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-black bg-opacity-50 backdrop-blur-md py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-gray-400">© {new Date().getFullYear()} Rex The Breathtaker. All rights reserved.</p>
+        <p className="text-center text-gray-400">© {new Date().getFullYear()} Mooshpea. All rights reserved.</p>
       </div>
     </footer>
   )
